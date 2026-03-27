95b78b8d89aac8a04b6ec7ac8b82b0bdc8453bd7
Aakash Kashyap <aakash.kash92@gmail.com>
2026-03-28 00:27:59 +0530

    Added: Header UI implement with basic styling and page routing between about, register, login.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,42 @@
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/search">Search</Link>
-      <Link to="/profile">Profile</Link>
+    <header className="bg-slate-300 shadow-md ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Brick</span>
+            <span className="text-slate-700">Nest</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-800 font-semibold hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="/register">
+            <li className="hidden sm:inline text-slate-800 font-semibold hover:underline cursor-pointer">
+              Register
+            </li>
+          </Link>
+          <Link to="/login">
+            <li className="text-slate-800 font-semibold hover:underline cursor-pointer">
+              Login
+            </li>
+          </Link>
+        </ul>
+      </div>
     </header>
   );
 };
