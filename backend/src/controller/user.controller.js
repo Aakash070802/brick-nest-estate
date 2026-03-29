6af517a2d2384c786fc4a4cc1fcb69264b7b54bb
Aakash Kashyap <aakash.kash92@gmail.com>
2026-03-29 12:21:04 +0530

    Added: updateUser controller but not tested.

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -6,22 +6,84 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 /**
  * @private getUser
  * @description Retrieves a user by their ID.
- * @GET /api/user/:id
+ * @GET /api/user/me
  */
 const getUser = async (req, res) => {
-  if (!req.user) {
-    throw new ApiError(401, "Unauthorized");
-  }
-
-  const user = await User.findById(req.user?._id).select(
-    "-password -refreshToken"
-  );
+  const user = req.user;
 
   if (!user) {
-    throw new ApiError(404, "Unauthorized Access, user not found!");
+    throw new ApiError(401, "Unauthorized Access, user not found!");
   }
 
   return res.status(200).json(new ApiResponse(200, "User Fetched!", user));
 };
 
-export { getUser };
+/**
+ * @private updateUser
+ * @description Updates a user's information by their ID.
+ * @body { username: string, email: string }
+ * @PATCH /api/user/me
+ */
+const updateUserController = async (req, res) => {
+  const user = req.user;
+  if (!user) {
+    throw new ApiError(401, "Unauthorized Access, user not found!");
+  }
+
+  const { username, email } = req.body;
+
+  if (!username && !email) {
+    throw new ApiError(400, "At least one field is required to update");
+  }
+
+  /* DUPLICATE CHECK */
+  if (email) {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser && existingUser._id.toString() !== user._id.toString()) {
+      throw new ApiError(409, "Email already in use");
+    }
+  }
+
+  if (username) {
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser && existingUser._id.toString() !== user._id.toString()) {
+      throw new ApiError(409, "Username already in use");
+    }
+  }
+
+  if (username) user.username = username;
+  if (email) user.email = email;
+
+  const updatedUser = await user.save();
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, "User Details Updated successfully!", updatedUser)
+    );
+};
+
+/**
+ * @private updateAvatar
+ * @description Updates a user's avatar by their ID.
+ * @body { avatar: string }
+ * @PATCH /api/user/me/avatar
+ */
+const updateAvatarController = async (req, res) => {};
+
+/**
+ * @private changePassword
+ * @description Changes a user's password by their ID.
+ * @body  { currentPassword: string, newPassword: string }
+ * @PATCH /api/user/me/password
+ */
+const changePasswordController = async (req, res) => {};
+
+export {
+  getUser,
+  updateUserController,
+  updateAvatarController,
+  changePasswordController,
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { getUser } from "../controller/user.controller.js";
+import {
+  changePasswordController,
+  getUser,
+  updateAvatarController,
+  updateUserController,
+} from "../controller/user.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
@@ -10,5 +15,29 @@ const router = Router();
  * @GET /api/user/me
  */
 router.route("/me").get(authMiddleware, getUser);
+
+/**
+ * @private updateUser
+ * @description Updates a user's information by their ID.
+ * @body { username: string, email: string }
+ * @PATCH /api/user/me
+ */
+router.route("/me").patch(authMiddleware, updateUserController);
+
+/**
+ * @private updateAvatar
+ * @description Updates a user's avatar by their ID.
+ * @body { avatar: string }
+ * @PATCH /api/user/me/avatar
+ */
+router.route("/me/avatar").patch(authMiddleware, updateAvatarController);
+
+/**
+ * @private changePassword
+ * @description Changes a user's password by their ID.
+ * @body  { currentPassword: string, newPassword: string }
+ * @PATCH /api/user/me/password
+ */
+router.route("/me/password").patch(authMiddleware, changePasswordController);
 
 export default router;
