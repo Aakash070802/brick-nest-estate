53785d97d315725860c217d6db4289443bb37a69
Aakash Kashyap <aakash.kash92@gmail.com>
2026-03-29 20:49:03 +0530

    Added: Listing model implemented.

---
+++ backend/src/models/listing.model.js
@@ -0,0 +1,64 @@
+import { Schema, model } from "mongoose";
+
+const listingSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+    },
+    regularPrice: {
+      type: Number,
+      required: [true, "Regular price is required"],
+    },
+    discountedPrice: {
+      type: Number,
+    },
+    bathrooms: {
+      type: Number,
+      required: [true, "Number of bathrooms is required"],
+    },
+    bedrooms: {
+      type: Number,
+      required: [true, "Number of bedrooms is required"],
+    },
+    furnished: {
+      type: Boolean,
+      required: [true, "Furnished status is required"],
+    },
+    parking: {
+      type: Boolean,
+      required: [true, "Parking status is required"],
+    },
+    type: {
+      type: String,
+      enum: ["rent", "sale"],
+      required: [true, "Type is required"],
+    },
+    offer: {
+      type: Boolean,
+      required: [true, "Offer status is required"],
+    },
+    imageUrls: [
+      {
+        type: String,
+        required: [true, "Image URL is required"],
+      },
+    ],
+    userRef: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User reference is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Listing = model("Listing", listingSchema);
