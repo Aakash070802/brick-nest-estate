afb52a4b1f76ad18814fcf2d89a03a9a5278a214
Aakash Kashyap <aakash.kash92@gmail.com>
2026-03-29 22:05:18 +0530

    Added: Otp model is implemented for restoring user account.

---
+++ backend/src/models/otp.model.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+
+const otpSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      index: true,
+    },
+
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    otpHash: {
+      type: String,
+      required: true,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+
+    purpose: {
+      type: String,
+      enum: ["restore-account", "reset-password"],
+      required: true,
+    },
+
+    attempts: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Otp = model("Otp", otpSchema);
