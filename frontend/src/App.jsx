373acfa5f1f063b99cb60260496886da3041914a
Aakash Kashyap <aakash.kash92@gmail.com>
2026-04-01 23:26:36 +0530

    Added: ThemeToggle feature.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useRoutes } from "react-router";
 import { routes } from "./routes/routesConfig";
 import Header from "./components/Header";
+import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   const routing = useRoutes(routes);
@@ -9,6 +10,14 @@ function App() {
     <>
       <Header />
       {routing}
+      <div className="bg-bg text-text min-h-screen p-10">
+        <ThemeToggle />
+
+        <div className="bg-card p-6 rounded-xl shadow-card mt-6">
+          <h1 className="text-2xl font-bold">BrickNest</h1>
+          <p className="text-muted">Theme working</p>
+        </div>
+      </div>
     </>
   );
 }
